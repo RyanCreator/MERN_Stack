a5f9c03954003b7154427a356c64fd16387a39ab
Ryan <50169457+Blackpepper-Creator@users.noreply.github.com>
2021-03-19 16:56:08 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 //server application
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -15,9 +16,9 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 //mongodb
-const CONNECTION_URL =
-  "mongodb+srv://MERN_Stack:1234@cluster0.bgkmc.mongodb.net/<dbname>?retryWrites=true&w=majority"; //cluster connect
+//const CONNECTION_URL = "mongodb+srv://MERN_Stack:1234@cluster0.bgkmc.mongodb.net/<dbname>?retryWrites=true&w=majority";  //cluster connect
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
